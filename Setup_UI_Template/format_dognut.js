5a76004e4e90fef82eba3d3c2b4fec9bd890b68f
Victor Stela <v.stela95@gmail.com>
2019-09-13 19:48:56 -0300

    Melhorias

--- Setup_UI_Template/format_dognut.js
+++ Setup_UI_Template/format_dognut.js
@@ -40,27 +40,27 @@ var totaltimeManutPlan = timeManutPlan.reduce(function(anterior, atual) {
     return anterior + atual;
 });
 
-var doughnut1 = [totaltimeNaoApontada+totaltimeUnscheduledStop,totaltimeScheduledStop,0,0,0,0];
-var doughnut2 = [0,0,totaltimeSetup,totaltimeManutProd,totaltimeRefeicao,totaltimeManutPlan];
+var doughnut1 = [totaltimeNaoApontada+totaltimeUnscheduledStop,totaltimeScheduledStop,0,0,0,0,0];
+var doughnut2 = [0,0,totaltimeNaoApontada,totaltimeSetup,totaltimeManutProd,totaltimeRefeicao,totaltimeManutPlan];
 
 var type = 'doughnut';
-var labels = ['Parada não planejada','Parada planejada','Setup','Ajuste / Regulagem','Refeição','Manutenção Planejada'];
+var labels = ['Parada não planejada','Parada planejada','Parada não apontada','Setup','Ajuste / Regulagem','Refeição','Manutenção Planejada'];
 var datasets = [{
     'type': 'doughnut',
     'label': 'Paradas',
     'data': doughnut1,
-    'backgroundColor': ['rgba(255, 59, 59, 0.5)','rgba(255, 229, 59, 0.5)','rgba(107, 255, 38, 0.5)','rgba(38, 255, 204, 0.5)','rgba(38, 42, 255, 0.5)','rgba(208, 38, 255, 0.5)'],
-    'borderColor': ['rgba(255, 59, 59, 1)','rgba(255, 229, 59, 1)','rgba(107, 255, 38, 1)','rgba(38, 255, 204, 1)','rgba(38, 42, 255, 1)','rgba(208, 38, 255, 1)'],
+    'backgroundColor': ['rgba(255, 59, 59, 0.5)','rgba(0, 163, 50, 0.5)','rgba(255, 167, 66, 0.5)','rgba(125, 54, 0, 0.5)','rgba(255, 204, 0, 0.5)','rgba(38, 42, 255, 0.5)','rgba(179, 255, 0, 0.5)'],
+    'borderColor': ['rgba(255, 59, 59, 1)','rgba(0, 163, 50, 1)','rgba(255, 167, 66, 1)','rgba(125, 54, 0, 1)','rgba(255, 204, 0, 1)','rgba(38, 42, 255, 1)','rgba(179, 255, 0, 1)'],
     'borderWidth': 1,
-    'hoverBackgroundColor': ['rgba(255, 59, 59, 1)','rgba(255, 229, 59, 1)','rgba(107, 255, 38, 1)','rgba(38, 255, 204, 1)','rgba(38, 42, 255, 1)','rgba(208, 38, 255, 1)']
+    'hoverBackgroundColor': ['rgba(255, 59, 59, 1)','rgba(0, 163, 50, 1)','rgba(255, 167, 66, 1)','rgba(125, 54, 0, 1)','rgba(255, 204, 0, 1)','rgba(38, 42, 255, 1)','rgba(179, 255, 0, 1)']
 },{
     'type': 'doughnut',
     'label': 'Paradas',
     'data': doughnut2,
-    'backgroundColor': ['rgba(255, 59, 59, 0.5)','rgba(255, 229, 59, 0.5)','rgba(107, 255, 38, 0.5)','rgba(38, 255, 204, 0.5)','rgba(38, 42, 255, 0.5)','rgba(208, 38, 255, 0.5)'],
-    'borderColor': ['rgba(255, 59, 59, 1)','rgba(255, 229, 59, 1)','rgba(107, 255, 38, 1)','rgba(38, 255, 204, 1)','rgba(38, 42, 255, 1)','rgba(208, 38, 255, 1)'],
+    'backgroundColor': ['rgba(255, 59, 59, 0.5)','rgba(0, 163, 50, 0.5)','rgba(255, 167, 66, 0.5)','rgba(125, 54, 0, 0.5)','rgba(255, 204, 0, 0.5)','rgba(38, 42, 255, 0.5)','rgba(179, 255, 0, 0.5)'],
+    'borderColor': ['rgba(255, 59, 59, 1)','rgba(0, 163, 50, 1)','rgba(255, 167, 66, 1)','rgba(125, 54, 0, 1)','rgba(255, 204, 0, 1)','rgba(38, 42, 255, 1)','rgba(179, 255, 0, 1)'],
     'borderWidth': 1,
-    'hoverBackgroundColor': ['rgba(255, 59, 59, 1)','rgba(255, 229, 59, 1)','rgba(107, 255, 38, 1)','rgba(38, 255, 204, 1)','rgba(38, 42, 255, 1)','rgba(208, 38, 255, 1)']
+    'hoverBackgroundColor': ['rgba(255, 59, 59, 1)','rgba(0, 163, 50, 1)','rgba(255, 167, 66, 1)','rgba(125, 54, 0, 1)','rgba(255, 204, 0, 1)','rgba(38, 42, 255, 1)','rgba(179, 255, 0, 1)']
 }
 ];
 var options = {
